cf36e83c1d4c10c0cfdf56349767a4093bb6bae0
monil_karia <monilkaria2019@gmail.com>
2024-09-13 09:58:02 +0530

    FORM Validation UserDefined Exceptions

---
+++ Form_validation_user_defined_exceptions/src/main/java/com/form_validator/api/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.form_validator.api.exception;
+
+public class UserNotFoundException  extends Exception{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
